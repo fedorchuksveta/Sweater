1649f5b289e26263f9e8e57b37728de31597b4fd
fedorchuksveta <fedrchuksveta@gmail.com>
2019-02-12 17:01:29 +0200

    Commit5

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,24 +1,30 @@
 package com.example.sweater;
 
 import com.example.sweater.entity.Remind;
+import com.example.sweater.repository.RemindRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class GreetingController {
+@Autowired
+private RemindRepository remindRepository;
+
+
 
     @GetMapping("/greeting")
-    public Remind greeting(
-            ) {
-       
-        return createMockremind();
+    public Remind greeting() {
+        List<Remind> list = remindRepository.findAll();
+        return createMockRemind();
     }
 
-    private Remind createMockremind() {
+    private Remind createMockRemind() {
         Remind remind = new Remind();
         remind.setId(1);
         remind.setRemindDate(new Date());
@@ -30,6 +36,6 @@ public class GreetingController {
     public String main(
         @RequestParam(name = "name", required = false, defaultValue="World") String name,Model model){
         model.addAttribute("name", "SWEATER");
-        return "main";
+        return "Hello Sweater";
     }
 }
---
+++ src/main/java/com/example/sweater/repository/RemindRepository.java
@@ -0,0 +1,7 @@
+package com.example.sweater.repository;
+
+import com.example.sweater.entity.Remind;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RemindRepository extends JpaRepository<Remind, Long> {
+}
